d0582aea5d141e728cb9b4439bba86fb64c37bfa
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2025-04-16 14:41:20 +0200

    Add files via upload

---
+++ __name__main/import_me.py
@@ -0,0 +1,8 @@
+def call_me():
+    print("Hello!!!")
+
+#call_me()
+
+#to prevent automatically execution of imported function, comment line 4
+if __name__ == '__main__':
+    call_me()
---
+++ __name__main/my_program.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from datetime import timezone
+
+df = pd.DataFrame([1, 2])
+
+print("this is a module")
+print(pd.__name__)
+
+print(timezone.__name__)
