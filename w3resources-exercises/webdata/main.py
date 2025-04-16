da7ec1514d1c4771058166985f32e6df7f98ebd1
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2025-04-16 14:53:08 +0200

    Delete w3resources-exercises/webdata directory

--- w3resources-exercises/webdata/main.py
+++
@@ -1,7 +0,0 @@
-# Source: https://www.w3resource.com/projects/python/python-projects-10.php
-import  webdata
-
-print("hello main")
-webdata.hello_webdata()
-result = webdata.get_data()
-print(result)
